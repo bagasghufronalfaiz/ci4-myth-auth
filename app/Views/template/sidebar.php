b82b8bfa54538d83d6d777ef471cd3abdeceaa2c
Bagas Ghufron Alfaiz <bagasghufronalfaiz@gmail.com>
2022-06-04 19:23:37 +0700

    admin login and register

---
+++ app/Views/template/sidebar.php
@@ -0,0 +1,49 @@
+<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+            <i class="fas fa-code"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin</div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <div class="sidebar-heading">
+        Profile
+    </div>
+
+    <!-- Nav Item - Profile -->
+    <li class="nav-item">
+        <a class="nav-link" href="index.html">
+            <i class="fas fa-user"></i>
+            <span>My Profile</span></a>
+    </li>
+
+    <!-- Nav Item - Edit Profile -->
+    <li class="nav-item">
+        <a class="nav-link" href="tables.html">
+            <i class="fas fa-user-edit"></i>
+            <span>Edit Profile</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <li class="nav-item">
+        <a class="nav-link" href="<?= base_url('logout'); ?>">
+            <i class="fas fa-sign-out-alt"></i>
+            <span>Log Out</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
---
+++ app/Views/user/index.php
@@ -0,0 +1,13 @@
+<?= $this->extend('template/index') ?>
+
+<?= $this->section('content') ?>
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+</div>
+<!-- End of Page Content -->
+
+<?= $this->endSection() ?>
